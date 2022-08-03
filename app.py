fd07f0f7859bb207d30f4ba139e896b9c6513fb2
alucaciu99 <110108703+alucaciu99@users.noreply.github.com>
2022-08-03 11:14:26 -0400

    a
    a

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import random
 ## Models Libraries
 import datetime
 import numpy as np
-#from codes.util_images import im_preprocess
+from codes.util_images import im_preprocess
 import cv2
 import base64
 
@@ -993,7 +993,6 @@ page_1_layout = html.Div([
                                                                     ])
                         ])
 ])
-
 
 
 
