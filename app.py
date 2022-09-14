7e3ca0b36cbf5ffa3538b55254d84e84bf466f57
alucaciu99 <110108703+alucaciu99@users.noreply.github.com>
2022-09-14 08:45:15 -0400

    Revert "Update app.py"
    This reverts commit 03689cb28d85e5cec2b4bc9177138bf1efa9deca.

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def login_button_click(n_clicks, username, password):
     print(list_surgeons)
 #create username for each primary surgeon using a loop
     USER_TO_NAME = {}
-    USER_LIST= {}.
+    USER_LIST= {}
 
     for x in list_surgeons:
         un = []
@@ -169,8 +169,7 @@ def login_button_click(n_clicks, username, password):
                 un = x[0] + x.rsplit(' ', 1)[1]
                 USER_TO_NAME.update({str(un) : x})
                 USER_LIST.update({str(un): (x[0: 2] + x[0:2])})
-                
-    print(USER_TO_NAME)
+    print(USER_LIST)
     #df = df[df['Primary Surgeon'] == USER_TO_NAME[username]]
     
     # we need this to account for empty pass code
